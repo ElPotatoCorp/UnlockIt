bfad1accc498c29cbe4e187cd6f6d3506ba235c7
Noah <noahgauche321@gmail.com>
2026-03-24 11:05:50 +0100

    Fix vite.config.ts

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import { join } from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+export default defineConfig(() => {
+  return {
+    envDir: join(process.cwd(), '..', '..'),
+    plugins: [
+      react(),
+      babel({ presets: [reactCompilerPreset()] })
+    ],
+    server: {
+      host: true,
+      port: 5175,
+    },
+  }
 })
