945a2ea14b9dae270cd25e0c8e45dd49ef23373e
Noah <noahgauche321@gmail.com>
2026-03-28 17:04:34 +0100

    Forgot a magic number

--- src/backend/src/docs/auth/decorators/api-auth.decorator.ts
+++ src/backend/src/docs/auth/decorators/api-auth.decorator.ts
@@ -1,8 +1,8 @@
 import { applyDecorators } from "@nestjs/common";
-import { ApiBearerAuth, ApiCookieAuth, ApiResponse } from "@nestjs/swagger";
+import { ApiBearerAuth, ApiCookieAuth, ApiUnauthorizedResponse } from "@nestjs/swagger";
 
 export const ApiAuth = () => applyDecorators(
   ApiCookieAuth('jwt'),
   ApiBearerAuth(),
-  ApiResponse({ status: 401, description: 'Unauthorized. No valid JWT cookie or Bearer token.' }),
+  ApiUnauthorizedResponse({ description: 'Unauthorized. No valid JWT cookie or Bearer token.' }),
 );
