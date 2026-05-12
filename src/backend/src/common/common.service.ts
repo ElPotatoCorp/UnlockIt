ca200b5fb056095d505d5b30c6f6b082bf2ee926
Noah <noahgauche321@gmail.com>
2026-05-12 11:44:56 +0200

    Add CommonModule and CommonService with pagination support

---
+++ src/backend/src/common/common.service.ts
@@ -0,0 +1,46 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { FindOptionsOrder, FindOptionsWhere, ObjectLiteral, Repository } from 'typeorm';
+import { PaginationQueryDto } from './dto/pagination-query.dto';
+import { PaginatedDto } from './dto/paginated.dto';
+
+export interface PaginatedResponseOptions<T extends ObjectLiteral, U> {
+  where?: FindOptionsWhere<T>;
+  transform?: (entity: T) => U;
+  order?: FindOptionsOrder<T>;
+}
+
+@Injectable()
+export class CommonService {
+  async getPaginatedResponse<T extends ObjectLiteral>(
+    repository: Repository<T>,
+    paginationQueryDto: PaginationQueryDto,
+  ): Promise<PaginatedDto<T>>;
+
+  async getPaginatedResponse<T extends ObjectLiteral, U>(
+    repository: Repository<T>,
+    paginationQueryDto: PaginationQueryDto,
+    options: PaginatedResponseOptions<T, U> & { transform: (entity: T) => U },
+  ): Promise<PaginatedDto<U>>;
+
+  async getPaginatedResponse<T extends ObjectLiteral, U>(
+    repository: Repository<T>,
+    paginationQueryDto: PaginationQueryDto,
+    options?: PaginatedResponseOptions<T, U>,
+  ): Promise<PaginatedDto<T | U>> {
+    const { page, limit } = paginationQueryDto;
+    const [data, total] = await repository.findAndCount({
+      where: options?.where,
+      order: options?.order,
+      skip: (page - 1) * limit,
+      take: limit,
+    });
+
+    if ((page - 1) * limit >= total && total > 0) {
+      throw new NotFoundException('Page not found');
+    }
+
+    const items = options?.transform ? data.map(options.transform) : data;
+
+    return new PaginatedDto(total, page, limit, items as (T | U)[]);
+  }
+}
