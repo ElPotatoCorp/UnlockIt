38c9920bf7b8d26ab36fcbce93d42a21a51c6508
Noah <noahgauche321@gmail.com>
2026-05-18 08:53:11 +0200

    Add EntityExistsPipe for entity existence validation in requests

---
+++ src/backend/src/common/pipes/entity-exists.pipe.ts
@@ -0,0 +1,42 @@
+import {
+  ArgumentMetadata,
+  Injectable,
+  mixin,
+  NotFoundException,
+  PipeTransform,
+  Type,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ObjectLiteral, Repository } from 'typeorm';
+
+export async function entityExists<T extends ObjectLiteral>(
+  repository: Repository<T>,
+  fieldName: keyof T = 'id' as keyof T,
+  value: any
+): Promise<T> {
+  const entity = await repository.findOne({
+    where: { [fieldName]: value } as any,
+  });
+
+  if (!entity) {
+    throw new NotFoundException(`${repository.metadata.name} with ${String(fieldName)} '${value}' not found`);
+  }
+
+  return entity;
+}
+
+export function EntityExistsPipe<T extends ObjectLiteral>(
+  entity: Type<T>,
+  field: keyof T = 'id' as keyof T,
+): Type<PipeTransform> {
+  @Injectable()
+  class EntityExistsMixin implements PipeTransform {
+    constructor(@InjectRepository(entity) private readonly repository: Repository<T>) {}
+
+    async transform(value: any, metadata: ArgumentMetadata): Promise<T> {
+      return entityExists(this.repository, field, value);
+    }
+  }
+
+  return mixin(EntityExistsMixin);
+}
