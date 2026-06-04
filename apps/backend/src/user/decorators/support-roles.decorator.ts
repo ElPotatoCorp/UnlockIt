d1e7604abd8e7e01f551c16656408a9271a96b38
Noah <noahgauche321@gmail.com>
2026-06-04 21:59:49 +0200

    Small fix

--- apps/backend/src/user/decorators/support-roles.decorator.ts
+++ apps/backend/src/user/decorators/support-roles.decorator.ts
@@ -1,4 +1,4 @@
 import { Reflector } from "@nestjs/core";
-import { EmployeeRole } from "../entities/employee.entity";
+import { EmployeeRole } from "@unlockit/shared";
 
 export const MinRole = Reflector.createDecorator<EmployeeRole>();
