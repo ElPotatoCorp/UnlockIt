62d4151064906663f6061c825e38e7f945330966
Noah <noahgauche321@gmail.com>
2026-06-07 05:13:23 +0200

    Types

--- packages/shared/src/utils/types.ts
+++ packages/shared/src/utils/types.ts
@@ -1,5 +1,9 @@
 export type Simplify<T> = { [K in keyof T]: T[K] } & {};
 
+export type UnwrapPromises<T> = {
+  [K in keyof T]: T[K] extends Promise<infer U> ? U : T[K];
+};
+
 type NullableKeys<T> = {
   [K in keyof T]: null extends T[K] ? K : never;
 }[keyof T];
@@ -9,6 +13,6 @@ type NonNullableKeys<T> = {
 }[keyof T];
 
 export type NullToOptional<T> = Simplify<
-    { [K in NonNullableKeys<T>]: T[K] } & 
-    { [K in NullableKeys<T>]?: Exclude<T[K], null> }
-  >;
+  { [K in NonNullableKeys<T>]: T[K] } & 
+  { [K in NullableKeys<T>]?: Exclude<T[K], null> }
+>;
