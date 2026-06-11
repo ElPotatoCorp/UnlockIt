5ac740e3830d3f4291a62059c22e81ef5ce10f63
Noah <noahgauche321@gmail.com>
2026-06-12 00:19:56 +0200

    Fix

--- packages/shared/src/game/game.types.ts
+++ packages/shared/src/game/game.types.ts
@@ -79,8 +79,6 @@ export type SearchGameOptions = {
   }
 }
 
-export type SearchBody = Omit<SearchGameOptions, 'name'>;
-
 export type AdvancedSearchGameOptions = Simplify<SearchGameOptions & {
   tags?: number[];
   developers?: number[];
