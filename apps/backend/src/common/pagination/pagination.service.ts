56fc9258ec18a1fda381949f579683e8720189d9
Noah <noahgauche321@gmail.com>
2026-06-17 03:38:34 +0200

    Cleanup pagination

--- apps/backend/src/common/pagination/pagination.service.ts
+++ apps/backend/src/common/pagination/pagination.service.ts
@@ -1,15 +1,14 @@
-import { FindOneOptions, ObjectLiteral, Repository } from "typeorm";
+import { FindOneOptions, ObjectLiteral, Repository, SelectQueryBuilder } from "typeorm";
 import { PaginationQueryDto } from "./dto/pagination-query.dto";
 import { PaginatedDto } from "./dto/paginated.dto";
 import { NotFoundException } from "@nestjs/common";
-import { SelectQueryBuilder } from "typeorm/browser";
 
-interface TransformEach<T, U> {
+type TransformEach<T, U> = {
   each?: true; // Transform each by default even when undefined is given
   fn: (entity: T) => U;
 }
 
-interface TransformAll<T, U> {
+type TransformAll<T, U> = {
   each: false;
   fn: (entities: T[]) => U;
 }
@@ -18,56 +17,55 @@ export type PaginatedResponseTransform<T, U> =
   | TransformEach<T, U>
   | TransformAll<T, U>;
 
-export interface PaginatedResponseOptions<
-  T extends ObjectLiteral,
-  U = T,
-> extends FindOneOptions<T> {
+export type PaginatedResponseOptions<T extends ObjectLiteral, U = T> =
+   FindOneOptions<T> & {
   transform?: PaginatedResponseTransform<T, U>;
 }
-
 
 export class PaginationService {
   public async getPaginatedResponse<T extends ObjectLiteral, U = T>(
     repository: Repository<T>,
     paginationQueryDto: PaginationQueryDto,
     options?: PaginatedResponseOptions<T, U>,
-  ): Promise<PaginatedDto<U>>
+  ): Promise<PaginatedDto<U>>;
 
   public async getPaginatedResponse<T extends ObjectLiteral, U = T>(
     queryBuilder: SelectQueryBuilder<T>,
     paginationQueryDto: PaginationQueryDto,
-    transform?: PaginatedResponseTransform<T,U>,
+    transform?: PaginatedResponseTransform<T, U>,
   ): Promise<PaginatedDto<U>>;
 
   public async getPaginatedResponse<T extends ObjectLiteral, U = T>(
     repositoryOrQuery: Repository<T> | SelectQueryBuilder<T>,
     paginationQueryDto: PaginationQueryDto,
-    optionsOrTransform?: PaginatedResponseOptions<T, U> | PaginatedResponseTransform<T,U>,
+    optionsOrTransform?: PaginatedResponseOptions<T, U> | PaginatedResponseTransform<T, U>,
   ): Promise<PaginatedDto<U>> {
     const { page, limit } = paginationQueryDto;
+    const offset = (page - 1) * limit;
 
-    let transform: PaginatedResponseTransform<T,U> | undefined;
-
+    let transform: PaginatedResponseTransform<T, U> | undefined;
     let data: T[];
     let total: number;
-    if (repositoryOrQuery instanceof Repository) {
-      transform = (optionsOrTransform as PaginatedResponseOptions<T,U> | undefined)?.transform;
 
-      [data, total] = await (repositoryOrQuery as Repository<T>).findAndCount({
-        ...optionsOrTransform,
-        skip: (page - 1) * limit,
+    if (repositoryOrQuery instanceof Repository) {
+      const { transform: _transform, ...findOptions } = (optionsOrTransform ?? {}) as PaginatedResponseOptions<T, U>;
+      transform = _transform;
+
+      [data, total] = await repositoryOrQuery.findAndCount({
+        ...findOptions,
+        skip: offset,
         take: limit,
       });
     } else {
-      transform = optionsOrTransform as PaginatedResponseTransform<T,U> | undefined;
+      transform = optionsOrTransform as PaginatedResponseTransform<T, U> | undefined;
 
-      [data, total] = await (repositoryOrQuery as SelectQueryBuilder<T>)
-        .skip((page - 1) * limit)
+      [data, total] = await repositoryOrQuery
+        .skip(offset)
         .take(limit)
         .getManyAndCount();
     }
 
-    if ((page - 1) * limit >= total && total > 0) {
+    if (offset >= total && total > 0) {
       throw new NotFoundException('Page not found');
     }
 
@@ -75,12 +73,12 @@ export class PaginationService {
     if (transform) {
       items =
         transform.each === false
-          ? [transform.fn(data)]
+          ? transform.fn(data) as U[]
           : data.map(transform.fn);
     } else {
       items = data as unknown as U[];
     }
 
     return new PaginatedDto(total, page, limit, items);
-  };
+  }
 }
