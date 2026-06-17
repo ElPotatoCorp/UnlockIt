7c37f59f09269a5d0da059a604748b95f96f14bd
Noah <noahgauche321@gmail.com>
2026-06-17 17:24:31 +0200

    Added a throwNotFound method in the commonService

--- apps/backend/src/common/entities/entities.service.ts
+++ apps/backend/src/common/entities/entities.service.ts
@@ -3,6 +3,8 @@ import { fetchEntityOrFail } from './pipes/fetch-entity.pipe';
 import { failIfDuplicated } from './pipes/duplicated-entry.pipe';
 import { entityExists } from './pipes/entity-exists.pipe';
 import { manyFailIfDuplicated } from './pipes/bulk-duplicated-entry.pipe';
+import { NotFoundException } from '@nestjs/common';
+import { buildNotFoundMessage } from './utils/helper';
 
 export class EntitiesService {
   public entityExists<T extends ObjectLiteral>(
@@ -33,5 +35,13 @@ export class EntitiesService {
     column: keyof T,
   ): Promise<void> {
     return manyFailIfDuplicated(repository, values, column);
+  }
+
+  public throwNotFound<T extends ObjectLiteral>(
+    repository: Repository<T>,
+    fields: (keyof T)[],
+    values: unknown[],
+  ): void {
+    throw new NotFoundException(buildNotFoundMessage(repository, fields, values));
   }
 }
