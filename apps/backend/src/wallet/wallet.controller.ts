2f8f16dae4767e9f11be9748aa8afd1bad2215fc
Noah <noahgauche321@gmail.com>
2026-06-21 00:19:43 +0200

    Docs

--- apps/backend/src/wallet/wallet.controller.ts
+++ apps/backend/src/wallet/wallet.controller.ts
@@ -3,16 +3,20 @@ import { WalletService } from './wallet.service';
 import { User } from 'src/user/decorators/user.decorator';
 import { PaginationQueryDto } from 'src/common/pagination/dto/pagination-query.dto';
 import { TopUpWalletDto } from './dto/top-up-wallet.dto';
+import { WalletControllerDoc } from 'src/docs/wallet/wallet.controller.doc';
 
+@WalletControllerDoc.Controller()
 @Controller('wallet')
 export class WalletController {
   constructor(private readonly walletService: WalletService) {}
 
+  @WalletControllerDoc.GetBalance()
   @Get()
   getBalance(@User('sub') userId: string) {
     return this.walletService.getBalance(userId);
   }
 
+  @WalletControllerDoc.GetTransactions()
   @Get('transactions')
   getTransactions(
     @User('sub') userId: string,
@@ -21,6 +25,7 @@ export class WalletController {
     return this.walletService.getTransactions(userId, paginationQuery);
   }
 
+  @WalletControllerDoc.TopUp()
   @Post('top-up')
   topUp(@User('sub') userId: string, @Body() dto: TopUpWalletDto) {
     return this.walletService.topUp(userId, dto);
