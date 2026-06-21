9c921967eaf4a481c3d17b3f83dc2b36c4c86ada
Noah <noahgauche321@gmail.com>
2026-06-21 22:46:24 +0200

    Docs

--- apps/backend/src/docs/auth/auth.controller.doc.ts
+++ apps/backend/src/docs/auth/auth.controller.doc.ts
@@ -173,6 +173,7 @@ export const AuthControllerDoc = {
           example: { resetToken: '7c9e6679-7425-40de-944b-e07fc1f90ae7' },
         },
       }),
+      ApiNotFoundResponse({ description: 'No user with this identifier.' }),
       ApiBadRequestResponse({ description: 'Validation failed.' }),
       ApiTooManyRequestsResponse({
         description: 'Too many password reset requests. Please try again later.',
