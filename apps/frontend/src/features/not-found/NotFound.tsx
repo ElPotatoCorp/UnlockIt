6ab19f4f1b4c08bfc5ac54cb888382493e6981b5
Frozen1753 <en424057@iut-dijon.u-bourgogne.fr>
2026-06-22 15:43:34 +0200

    add css to notfound page

--- apps/frontend/src/features/not-found/NotFound.tsx
+++ apps/frontend/src/features/not-found/NotFound.tsx
@@ -1,32 +1,28 @@
 import { type FC } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import styles from "./notFound.module.css";
 import { Card } from "../../components/common/card/Card";
+import { Button } from "../../components/common/button/Button";
 
 export const NotFound: FC = () => {
+    const navigate = useNavigate();
+
     return (
         <div className={styles.notFoundPage}>
-            <h1 className={styles.pageTitle}>Erreur 404</h1>
+            <h1 className={styles.errorCode}>404</h1>
 
-            <Card className={styles.card}>
-                <h2>Page introuvable</h2>
+            <Card>
+                <div className={styles.cardContent}>
+                    <h2>Page introuvable</h2>
 
-                <p>
-                    Oups… Il semble que cette page n’existe pas ou a été déplacée.
-                </p>
+                    <p className={styles.message}>
+                        Oups... Cette page n’existe pas ou a été déplacée.
+                    </p>
 
-                <ul>
-                    <li>
-                        <Link to="/">
-                            Retour à l’accueil
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/login">
-                            Connexion
-                        </Link>
-                    </li>
-                </ul>
+                    <Button onClick={() => navigate("/")}>
+                        Retour à l’accueil
+                    </Button>
+                </div>
             </Card>
         </div>
     );
